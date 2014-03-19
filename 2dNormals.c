970028247b04d79f1bea8262050a18ff7c92cae6
bcieslak <bcieslak@lclark.edu>
2014-03-18 20:36:36 -0700

    2d Normals are Working As Intended.

--- 2dNormals.c
+++ 2dNormals.c
@@ -78,92 +78,43 @@ int lemon (double p[2], double u) {
   p[1] = sin(u);
 }
 
-int drawNormal(int (*func)(double[2], double), double u, double padd[2]) {
+int findNormal(double p1[2], double p2[2], double padd[2]) {
   //Find the tangent line at a point and then find the perpendicular line
-  double newu;
   double xslope;
   double yslope;
-  double p1[2], p2[2];
-  double m;
-  double xamt;
-  newu = u + 0.0001;
-  (*func)(p1, u);
-  (*func)(p2, newu);
+  double uvec;
   xslope = p2[0] - p1[0];
   yslope = p2[1] - p1[1];
-  if(u <= M_PI / 2) {
-    if(yslope == 0) {
-      padd[0] = p1[0];
-      padd[1] = p1[1] + 10;
-    } else if(xslope == 0) {
-      padd[0] = p1[0] + 10;
-      padd[1] = p1[1];
-    } else {
-      m = -1/(yslope/xslope);
-      xamt = sqrt(1 / (pow(m, 2) + 1));
-      padd[0] = p1[0] + xamt;
-      padd[1] = p1[1] + m * xamt;
-    }
-  } else if(u <= M_PI) {
-    if(yslope == 0) {
-      padd[0] = p1[0];
-      padd[1] = p1[1] + 10;
-    } else if(xslope == 0) {
-      padd[0] = p1[0] - 10;
-      padd[1] = p1[1];
-    } else {
-      m = -1/(yslope/xslope);
-      xamt = sqrt(1 / (pow(m, 2) + 1));
-      padd[0] = p1[0] - xamt;
-      padd[1] = p1[1] + m * xamt;
-    }
-  } else if(u <= M_PI * 3/2) {
-    if(yslope == 0) {
-      padd[0] = p1[0];
-      padd[1] = p1[1] - 10;
-    } else if(xslope == 0) {
-      padd[0] = p1[0] - 10;
-      padd[1] = p1[1];
-    } else {
-      m = -1/(yslope/xslope);
-      xamt = sqrt(1 / (pow(m, 2) + 1));
-      padd[0] = p1[0] - xamt;
-      padd[1] = p1[1] - m * xamt;
-    }
-  } else {
-    if(yslope == 0) {
-      padd[0] = p1[0];
-      padd[1] = p1[1] - 10;
-    } else if(xslope == 0) {
-      padd[0] = p1[0] + 10;
-      padd[1] = p1[1];
-    } else {
-      m = -1/(yslope/xslope);
-      xamt = sqrt(1 / (pow(m, 2) + 1));
-      padd[0] = p1[0] + xamt;
-      padd[1] = p1[1] - m * xamt;
-    }
-  }
-}   
+  uvec = sqrt(pow(xslope, 2) + pow(yslope, 2));
+  xslope = xslope / uvec;
+  yslope = yslope / uvec;
+  padd[0] = p1[0] + (yslope) * 25;
+  padd[1] = p1[1] - (xslope) * 25;
+}
 
 void graph(int (*func)(double[2], double), double m[4][4], double minv[4][4],
-	double start, double end, double increment) {
-    double p[2];
-    double p2[2];
-    double u;
+	   double start, double end, double increment) {
+  double p[2];
+  double p2[2];
+    double pn[2];
+    double u, u2;
     int i;
     printf("Graphing. Start is %lf, end is %lf, increment is %lf.\n", start, end, increment);
     i = 0;
     for(u = start; u <= end; u+= increment) {
-    	(*func)(p, u);
-    	D3d_mat_mult_pt(p, m, p);
-    	G_point(p[0], p[1]);
-	if(i % 150 == 0) {
-	  drawNormal(&(*func),u, p2);
-	  D3d_mat_mult_pt(p2, m, p2);
-	  G_line(p[0], p[1], p2[0], p2[1]);
-	}
-	i++;
+      u2 = u + 0.00001;
+      (*func)(p, u);
+      (*func)(p2, u2);
+      D3d_mat_mult_pt(p, m, p);
+      D3d_mat_mult_pt(p2, m, p2);
+      G_point(p[0], p[1]);
+      if(i % 150 == 0) {
+	findNormal(p, p2, pn);
+	G_rgb(0,0,1);
+	G_line(p[0], p[1], pn[0], pn[1]);
+	G_rgb(1,0,0);
+      }
+      i++;
     }
 }
     
@@ -176,8 +127,8 @@ int main() {
     G_init_graphics(600,600);
     G_rgb(0,0,0);
     G_clear();
-
-	printf("Circle\n");
+    
+    printf("Circle\n");
     //Making the Circle
     num_movements = 4;
     mt_list[0] = SX; p_list[0] = 50.0;
@@ -187,12 +138,12 @@ int main() {
     D3d_make_movement_sequence_matrix(m, minv, num_movements, mt_list, p_list);
     G_rgb(1,0,0);
     graph(&circle, m, minv, 0.25 * M_PI, 1.50 * M_PI, P_INCREMENT);
-
+    
     G_wait_key();
     G_rgb(0,0,0);
     G_clear();
-
-printf("Sum4\n");
+    
+    printf("Sum4\n");
     //Making the sum4
     num_movements = 4;
     mt_list[0] = SX; p_list[0] = 30.0;
@@ -202,12 +153,12 @@ printf("Sum4\n");
     D3d_make_movement_sequence_matrix(m, minv, num_movements, mt_list, p_list);
     G_rgb(1,0,0);
     graph(&sum4, m, minv, 0.50 * M_PI, 1.75 * M_PI, P_INCREMENT);
-
+    
     G_wait_key();
     G_rgb(0,0,0);
     G_clear();
-
-printf("Square\n");
+    
+    printf("Square\n");
     //Making the Square
     num_movements = 4;
     mt_list[0] = SX; p_list[0] = 150.0;
@@ -217,12 +168,12 @@ printf("Square\n");
     D3d_make_movement_sequence_matrix(m, minv, num_movements, mt_list, p_list);
     G_rgb(1,0,0);
     graph(&square, m, minv, 0, 2.0 * M_PI, P_INCREMENT);
-
+    
     G_wait_key();
     G_rgb(0,0,0);
     G_clear();
-
-printf("Asteroid\n");
+    
+    printf("Asteroid\n");
     //Making the Asteroid
     num_movements = 5;
     mt_list[0] = SX; p_list[0] = 80.0;
@@ -233,12 +184,12 @@ printf("Asteroid\n");
     D3d_make_movement_sequence_matrix(m, minv, num_movements, mt_list, p_list);
     G_rgb(1,0,0);
     graph(&asteroid, m, minv, 0, 2.0 * M_PI, P_INCREMENT);
-
+    
     G_wait_key();
     G_rgb(0,0,0);
     G_clear();
-
-printf("Hyperbola\n");
+    
+    printf("Hyperbola\n");
     //Making the Hyperbola
     num_movements = 5;
     mt_list[0] = NY; p_list[0] = 0.0;
@@ -249,12 +200,12 @@ printf("Hyperbola\n");
     D3d_make_movement_sequence_matrix(m, minv, num_movements, mt_list, p_list);
     G_rgb(1,0,0);
     graph(&hyperbola, m, minv, -1, 2, P_INCREMENT);
-
+    
     G_wait_key();
     G_rgb(0,0,0);
     G_clear();
-
-printf("Parabola\n");
+    
+    printf("Parabola\n");
     //Making the Parabola
     num_movements = 5;
     mt_list[0] = SX; p_list[0] = 150.0;
@@ -265,12 +216,12 @@ printf("Parabola\n");
     D3d_make_movement_sequence_matrix(m, minv, num_movements, mt_list, p_list);
     G_rgb(1,0,0);
     graph(&parabola, m, minv, -1, 2, P_INCREMENT);
-
+    
     G_wait_key();
     G_rgb(0,0,0);
     G_clear();
-
-printf("Lemon\n");
+    
+    printf("Lemon\n");
     //Making of Lemon
     num_movements = 5;
     mt_list[0] = SX; p_list[0] = 150.0;
@@ -281,6 +232,6 @@ printf("Lemon\n");
     D3d_make_movement_sequence_matrix(m, minv, num_movements, mt_list, p_list);
     G_rgb(1,0,0);
     graph(&lemon, m, minv, 0, 2.0 * M_PI, P_INCREMENT);
-
+    
     G_wait_key();
 }
